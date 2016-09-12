ba0de7a64049ad6e26c0eff5724f7822b960f0a7
Tony Chen <topsbuilder@gmail.com>
2016-09-12 00:31:21 -0400

    switch db to mongolab

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,8 +7,8 @@ module.exports = {
   mongo: {
     //uri: 'mongodb://localhost/shopnx-dev'
 	//uri: 'mongodb://mitx:mitx@ds031812.mongolab.com:31812/mitxdb'
-	//uri: 'mongodb://mitx:mitx@ds019816.mlab.com:19816/shopnx-dev'
-	uri: 'mongodb://u.mitx.ca:27017/shopnx-dev'
+	uri: 'mongodb://mitx:mitx@ds019816.mlab.com:19816/shopnx-dev'
+	//uri: 'mongodb://u.mitx.ca:27017/shopnx-dev'
   },
 
   //seedDB: true
